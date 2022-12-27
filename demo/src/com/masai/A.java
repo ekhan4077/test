ec4d967a90abf337729d7651e7635c54575d64df
ekhan4077 <ekhan4077@gmail.com>
2022-12-27 11:44:03 +0530

    testing2

--- demo/src/com/masai/A.java
+++ demo/src/com/masai/A.java
@@ -2,6 +2,6 @@ package com.masai;
 
 public class A {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World!");
     }
 }
